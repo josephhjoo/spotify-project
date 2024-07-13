ab2ed6f8e7f14ae07173d8352d61c38efef99888
Joseph Joo <josephjoo@Josephs-MacBook-Air.local>
2024-07-12 20:30:56 -0700

    071224

--- spotify.py
+++ spotify.py
@@ -3,8 +3,6 @@ import numpy as np
 
 # column index: 0 (Spotify ID), 1 (Artist IDs) , 2 (Track Name) , 3 (Album Name), 4 (Artist Name(s)), 5 (Release Date), 6 (Duration (ms)), 7 (Popularity), 8 (Added By), 9 (Added At), 10 (Genres), 11 (Danceability), 12 (Energy), 13 (Key), 14 (Loudness), 15 (Mode), 16 (Speechiness), 17 (Acousticness), 18 (Instrumentalness), 19 (Liveness), 20 (Valence), 21 (Tempo), 22 (Time Signature)
 
-# Valence = measure of happiness
-
 spotify = pd.read_csv('your_top_songs_2023.csv')
 
 def track_and_artist(data_row):
@@ -22,10 +20,17 @@ def table_select_3_attributes(data_table, v1, v2, v3):
 def songs_by_attributes(data_table, v1, v2, v3, k):
     # returns table with k rows that relate most to three attributes of choice (v1, v2, v3) from data_table
     relevant_table = data_table.loc[:, ['Track Name', 'Artist Name(s)', v1, v2, v3]]
-    distance_columm = distance_3d(100, 100, 100, relevant_table[v1] * 100, relevant_table[v2] * 100, relevant_table[v3] * 100)
+    # distance_columm = distance_3d(100, 100, 100, relevant_table[v1] * 100, relevant_table[v2] * 100, relevant_table[v3] * 100)
     table_with_distances = relevant_table.assign(distances = distance_columm)
     sorted_table = table_with_distances.sort_values('distances').head(k)
     songs_and_artists = sorted_table.loc[:, ['Track Name', 'Artist Name(s)']]
     return songs_and_artists
 
-print(songs_by_attributes(spotify, 'Popularity', 'Energy', 'Valence', 5))
+def songs_by_one_attribute(data_table, att, k):
+    # returns table with k rows that relate most to one attribute of choice (att) from data_table
+    relevant_table = data_table.loc[:, ['Track Name', 'Artist Name(s)', att]]
+    sorted_table = relevant_table.sort_values(att, ascending = False).head(k)
+    songs_and_artists = sorted_table.loc[:, ['Track Name', 'Artist Name(s)']]
+    return songs_and_artists
+
+print(songs_by_one_attribute(spotify, 'Instrumentalness', 5))
