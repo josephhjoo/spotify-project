bc6fd5c98502021ea6ede32696bb4da00a16919c
josephhjoo <josephjoo@berkeley.edu>
2024-09-20 14:25:20 -0700

    all files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import re
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/indie")
+def indie():
+    render_template("indie.html")
+    header = "this is an indie playlist generator"
+    return render_template("indie.html")
+
+@app.route("/house")
+def house():
+    header = "this is a house playlist generator"
+    return header
+
